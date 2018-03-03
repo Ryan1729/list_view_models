d2d70f902224382dcc22e8fbf35f0826db92bc71
Ryan1729 <Ryan1729@gmail.com>
2018-03-03 15:19:30 -0700

    basic mostly-working version
    Signed-off-by: Ryan1729 <Ryan1729@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,39 @@
 use std::collections::HashMap;
 use std::ffi::OsStr;
 
-fn main() {
+fn main()  {
     let mut view_models: HashMap<String, String> = HashMap::new();
 
-    let cb = |dir: &DirEntry| {
-        let path = dir.path();
-        if let Some("cs") = path.extension().and_then(OsStr::to_str) {
-            let mut lines = get_lines(path);
+    {
+        let mut cb = |dir: &DirEntry| {
+            let path = dir.path();
+            if let Some("cs") = path.extension().and_then(OsStr::to_str) {
+                let mut lines = get_lines(path).unwrap().map(|result| { result.unwrap() });
 
-            for line in lines {
-                if let view_model_name = extract_view_model_name(line) {
-                    let view_model_text = String::new();
+                while let Some(line) = lines.next() {
+                    if let Some(view_model_name) = extract_view_model_name(&line) {
+                        let mut view_model_text = String::new();
 
-                    view_model_text.push(line);
+                        view_model_text.push_str(&line);
+                        view_model_text.push('\n');
 
-                    while let Some(view_model_line) = lines.next() {
-                        view_model_text.push(view_model_line);
+                        while let Some(view_model_line) = lines.next() {
+                            view_model_text.push_str(&view_model_line);
+                            view_model_text.push('\n');
 
-                        if is_view_model_end(view_model_line) {
-                            break;
+                            if is_view_model_end(&view_model_line) {
+                                break;
+                            }
                         }
-                    }
 
-                    view_models.add(view_model_name, view_model_text);
+                        view_models.insert(view_model_name, view_model_text);
+                    }
                 }
             }
-        }
-    };
+        };
 
-    visit_dirs(Path::new("."), cb);
-
+        visit_dirs(Path::new("."), &mut cb).unwrap();
+    }
     let mut pairs: Vec<_> = view_models.iter().collect();
 
     pairs.sort();
@@ -42,13 +45,81 @@ fn main() {
     }
 }
 
-use std::io;
+#[macro_use] extern crate lazy_static;
+extern crate regex;
+use regex::Regex;
+
+fn extract_view_model_name(line: &str) -> Option<String> {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"class\s+([A-Za-z_]*ViewModel)\s*").unwrap();
+    }
+
+    RE.captures_iter(line).map(|c| c[1].to_string()).next()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_extract_view_model_name_in_best_case() {
+        assert_eq!(
+            Some("CheeseViewModel".to_string()),
+            extract_view_model_name("class CheeseViewModel")
+        );
+    }
+
+    #[test]
+    fn test_extract_view_model_name_with_full_line() {
+        assert_eq!(
+            Some("CheeseViewModel".to_string()),
+            extract_view_model_name("    public class CheeseViewModel {")
+        );
+    }
+
+    #[test]
+    fn test_extract_view_model_name_with_base_class() {
+        assert_eq!(
+            Some("CheeseViewModel".to_string()),
+            extract_view_model_name("    public class CheeseViewModel : MilkViewModelBase {")
+        );
+    }
+
+    #[test]
+    #[allow(non_snake_case)]
+    fn test_extract_view_model_name_does_not_extract_ViewModelMapping() {
+        assert_ne!(
+            Some("CheeseViewModel".to_string()),
+            extract_view_model_name("public static class ViewModelMapping")
+        );
+    }
+}
+
+fn is_view_model_end(line: &str) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^\s*\}\s*$").unwrap();
+    }
+
+    RE.is_match(line)
+}
+
+use std::io::{self, BufReader, Lines};
+use std::io::prelude::*;
+use std::fs::File;
+
+fn get_lines<P : AsRef<Path>>(path: P) -> io::Result<Lines<BufReader<File>>> {
+    let f = File::open(path)?;
+    let f = BufReader::new(f);
+
+    Ok(f.lines())
+}
+
 use std::fs::{self, DirEntry};
 use std::path::Path;
 
 // one possible implementation of walking a directory only visiting files
 //(from the rust docs)
-fn visit_dirs(dir: &Path, cb: &Fn(&DirEntry)) -> io::Result<()> {
+fn visit_dirs(dir: &Path, cb: &mut FnMut(&DirEntry)) -> io::Result<()> {
     if dir.is_dir() {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
